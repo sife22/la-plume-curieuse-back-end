e914fff39ac325e0194230bbf026e3c74e39ad3e
sife22 <sifeddinehadi22@gmail.com>
2024-09-02 05:50:56 +0100

    add newsletter, subscribe function

---
+++ app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Newsletter;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Validator;
+
+class NewsletterController extends Controller
+{
+    public function subscribe(Request $request){
+
+        // on valide les données récupérées
+        $request->validate([
+            'email'=>'required|email|unique:newsletter,email',
+        ],[
+            'email.required'=>"L'email est requis !",
+            'email.email'=>"Ex: contact@gmail.com",
+            'email.unique'=>"Vous êtes déjà inscrit !"
+        ]);
+
+        // on enregiste l'email
+        $new_email = new Newsletter();
+        $new_email->email = $request['email'];
+        $new_email->created_at = Carbon::now();
+        $new_email->save();
+
+        return response()->json([''=>''], 201);
+    }
+}
